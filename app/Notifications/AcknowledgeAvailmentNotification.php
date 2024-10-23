2d4894efd141de0579a39c4a9a1e0f742d06b7b9
jn-devops <devops@joy-nostalg.com>
2024-10-23 13:31:23 +0800

    update notification

--- app/Notifications/AcknowledgeAvailmentNotification.php
+++ app/Notifications/AcknowledgeAvailmentNotification.php
@@ -3,5 +3,20 @@
 namespace App\Notifications;
 
 use LBHurtado\EngageSpark\Notifications\Adhoc as BaseAdhoc;
+use App\Actions\ProcessFeedback;
+use App\Models\Checkin;
 
-class AcknowledgeAvailmentNotification extends BaseAdhoc{}
+class AcknowledgeAvailmentNotification extends BaseAdhoc
+{
+    public function __construct(Checkin $checkin)
+    {
+        $default = 'Thank you for checking in. Here is your check-in reference code: '.substr($checkin->contact->id, -12).'
+
+If you are ready to secure the unit, kindly click on the link provided below:
+https://www.homeful.ph/pagsikat/availnow/promocode=?pasinaya'.substr($checkin->contact->id, -12);
+
+        $message = ProcessFeedback::run($checkin, $checkin->campaign->feedback ?: $default);
+
+        parent::__construct($message);
+    }
+}
