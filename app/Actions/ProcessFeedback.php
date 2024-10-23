cc96ae84e1dd7582c97c48f9ce5e65a7fdc2b096
jn-devops <devops@joy-nostalg.com>
2024-10-23 13:23:21 +0800

    add process feedback

---
+++ app/Actions/ProcessFeedback.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+use App\Models\Checkin;
+use App\Models\Contact;
+
+class ProcessFeedback
+{
+    use AsAction;
+
+    public function handle(Checkin|string $checkin, string $feedback): string
+    {
+        $checkin = $checkin instanceof Checkin ? $checkin : Checkin::where('id', $checkin)->first();
+        if ($checkin instanceof Checkin) {
+            $contact = $checkin->contact;
+            return __($feedback, [
+                'mobile' => phone($contact->mobile, 'PH')->formatNational(),
+                'name' => $contact->name,
+                'organization' => $contact->organization->name,
+                'campaign' => $checkin->campaign->name
+            ]);
+        }
+    }
+}
