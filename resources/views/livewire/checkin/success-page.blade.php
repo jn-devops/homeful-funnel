122f082c2a06316a9c11240a93168c18ed437297
Christian Quelonio <cequelonio@joy-nostalg.com>
2024-10-28 10:07:59 +0800

    added text on avail button

--- resources/views/livewire/checkin/success-page.blade.php
+++ resources/views/livewire/checkin/success-page.blade.php
@@ -8,8 +8,6 @@
                 <div>Hi {{$checkin->contact->first_name ?? ''}}, Thank you for your Registration from {{$checkin->contact->organization->name??''}}. </div>
                 <br>
                 <div>Here is your registration code: </div>
-                <br>
-                <br>
                 <div class="font-semibold text-3xl">{{ substr($checkin->contact->id, -12) ?? ''}}</div>
             </div>
             <div class="flex flex-col items-center space-y-4" style="margin-left: 2rem; margin-right: 2rem;">
@@ -18,14 +16,15 @@
                             {{-- style="background-color: white; border: 1px solid rgb(212, 212, 212);"> --}}
                         {{-- Close --}}
                     {{-- </button> --}}
-                <div style="color: rgb(30 64 175); padding: 0.5rem; background-color: rgb(219 234 254); font-size: 0.8rem" class="rounded-lg text-center w-full"> You may use your code to waive the P10,000 Consultation Fee</div>
+                {{-- <div style="color: rgb(30 64 175); padding: 0.5rem; background-color: rgb(219 234 254); font-size: 0.8rem" class="rounded-lg text-center w-full"> You may use your code to waive the P10,000 Consultation Fee</div> --}}
                <div class="flex flex-col items-center mb-4 w-full">
-                   {{-- <span class="text-center mb-1">Should you wish to continue? Click</span> --}}
+                   <span class="text-center mb-1">Should you wish to continue? Click</span>
                    <button wire:click="redirect_page_to('{{$checkin->redirect_url}}')"
                            class="w-full max-w-xs rounded-lg text-white font-bold p-2"
                            style="background-color: #D97706;">
                        Avail Now
                    </button>
+                   <span class="text-center">Use the registration code to avail</span>
                </div>
                <div class="flex flex-col items-center w-full">
                    {{-- <span class="text-center">Use Registration code to avail</span> --}}
