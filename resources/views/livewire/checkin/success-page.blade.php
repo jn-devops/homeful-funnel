54ecb26a9e3b8650f3e03eb4d43d35701954bd02
Christian Quelonio <cequelonio@joy-nostalg.com>
2024-10-28 09:59:43 +0800

    update button

--- resources/views/livewire/checkin/success-page.blade.php
+++ resources/views/livewire/checkin/success-page.blade.php
@@ -3,7 +3,7 @@
     <div class="flex justify-center items-center">
         <div class="" style="margin-top: 6rem">
             <div id="lottie-animation" class=" mx-auto" style="width: 12rem;"></div>
-            <div class="text-center mx-5 mb-16">
+            <div class="text-center mx-5 mb-10">
                 <div class="text-3xl font-bold mb-4">Successfully Registered</div>
                 <div>Hi {{$checkin->contact->first_name ?? ''}}, Thank you for your Registration from {{$checkin->contact->organization->name??''}}. </div>
                 <br>
@@ -12,33 +12,34 @@
                 <br>
                 <div class="font-semibold text-3xl">{{ substr($checkin->contact->id, -12) ?? ''}}</div>
             </div>
-            <div class="flex flex-col items-center space-y-4" style="margin-left: 3.5rem; margin-right: 3.5rem;">
-                    <button wire:click="redirect_page_to('{{$checkin->campaign->rider_url}}')"
-                            class="w-full max-w-xs rounded-lg text-black font-bold p-2 mt-1"
-                            style="background-color: white; border: 1px solid rgb(212, 212, 212);">
-                        Close
-                    </button>
-{{--                <div class="flex flex-col items-center mb-4">--}}
-{{--                    <span class="text-center mb-1">Should you wish to continue? Click</span>--}}
-{{--                    <button wire:click="redirect_page_to('{{$checkin->redirect_url}}')"--}}
-{{--                            class="w-full max-w-xs rounded-lg text-white font-bold p-2"--}}
-{{--                            style="background-color: #D97706;">--}}
-{{--                        Avail Now--}}
-{{--                    </button>--}}
-{{--                </div>--}}
-{{--                <div class="flex flex-col items-center">--}}
-{{--                    <span class="text-center">Use Registration code to avail</span>--}}
-{{--                    <button wire:click="redirect_page_to('/checkin/{{$checkin->campaign->id}}/{{$checkin->contact->organization->id}}')"--}}
-{{--                            class="w-full max-w-xs rounded-lg text-black font-bold p-2 mt-1"--}}
-{{--                            style="background-color: white; border: 1px solid rgb(212, 212, 212);">--}}
-{{--                        Not Now--}}
-{{--                    </button>--}}
-{{--                    <button wire:click="redirect_page_to('https://homeful.ph/')"--}}
-{{--                            class="w-full max-w-xs rounded-lg text-black font-bold p-2 mt-1"--}}
-{{--                            style="background-color: white; border: 1px solid rgb(212, 212, 212);">--}}
-{{--                        Not Now--}}
-{{--                    </button>--}}
-{{--                </div>--}}
+            <div class="flex flex-col items-center space-y-4" style="margin-left: 2rem; margin-right: 2rem;">
+                    {{-- <button wire:click="redirect_page_to('{{$checkin->campaign->rider_url}}')" --}}
+                            {{-- class="w-full max-w-xs rounded-lg text-black font-bold p-2 mt-1" --}}
+                            {{-- style="background-color: white; border: 1px solid rgb(212, 212, 212);"> --}}
+                        {{-- Close --}}
+                    {{-- </button> --}}
+                <div style="color: rgb(30 64 175); padding: 0.5rem; background-color: rgb(219 234 254); font-size: 0.8rem" class="rounded-lg text-center w-full"> You may use your code to waive the P10,000 Consultation Fee</div>
+               <div class="flex flex-col items-center mb-4 w-full">
+                   {{-- <span class="text-center mb-1">Should you wish to continue? Click</span> --}}
+                   <button wire:click="redirect_page_to('{{$checkin->redirect_url}}')"
+                           class="w-full max-w-xs rounded-lg text-white font-bold p-2"
+                           style="background-color: #D97706;">
+                       Avail Now
+                   </button>
+               </div>
+               <div class="flex flex-col items-center w-full">
+                   {{-- <span class="text-center">Use Registration code to avail</span> --}}
+                   <button wire:click="redirect_page_to('/checkin/{{$checkin->campaign->id}}/{{$checkin->contact->organization->id}}')"
+                           class="w-full max-w-xs rounded-lg text-black font-bold p-2 mt-1"
+                           style="background-color: white; border: 1px solid rgb(212, 212, 212);">
+                       Not Now
+                   </button>
+                   {{-- <button wire:click="redirect_page_to('https://homeful.ph/')" --}}
+                           {{-- class="w-full max-w-xs rounded-lg text-black font-bold p-2 mt-1" --}}
+                           {{-- style="background-color: white; border: 1px solid rgb(212, 212, 212);"> --}}
+                       {{-- Not Now --}}
+                   {{-- </button> --}}
+               </div>
 
             </div>
 
