0607b73fea0451b83931814e890378cd9ebd90a5
renzoDC15 <renzo.carianga@gmail.com>
2024-10-28 14:19:50 +0800

    add params to avail now link

--- resources/views/livewire/checkin/success-page.blade.php
+++ resources/views/livewire/checkin/success-page.blade.php
@@ -19,7 +19,7 @@
                 {{-- <div style="color: rgb(30 64 175); padding: 0.5rem; background-color: rgb(219 234 254); font-size: 0.8rem" class="rounded-lg text-center w-full"> You may use your code to waive the P10,000 Consultation Fee</div> --}}
                <div class="flex flex-col items-center w-full">
                    <span class="text-center mb-1">Should you wish to continue? Click</span>
-                   <button wire:click="redirect_page_to('{{config('kwyc-check.campaign_url')}}')"
+                   <button wire:click="redirect_page_to('{{ config('kwyc-check.campaign_url') . '?email=' . $checkin->contact->email . '&mobile=' . $checkin->contact->mobile . '&identifier='.$checkin->registration_code.'&code=ABC123&choice=JN-PPMP-HLTH-IU' }}')"
                            class="w-full max-w-xs rounded-lg text-white font-bold p-2"
                            style="background-color: #D97706;">
                        Avail Now
