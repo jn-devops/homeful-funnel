a999c6cbef231eee4a9c6b619ee54894c927f10f
renzoDC15 <renzo.carianga@gmail.com>
2024-11-11 11:04:05 +0800

    add state to Trips Model

--- app/Models/Trips.php
+++ app/Models/Trips.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\ModelStates\HasStates;
 
 class Trips extends Model
 {
+    use HasStates;
     protected $fillable = [
         'campaign_id',
         'contact_id',
